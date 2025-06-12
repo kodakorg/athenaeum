8f60892958bc7b79ff858d6a3ae7b177ecb15270
Ole Severin Hustad <ole.severin.hustad@basefarm-orange.com>
2025-06-12 14:52:18 +0200

    Update morgan config

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use('/css', express.static(path.join(__dirname, 'public/css')))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan('common'));
+app.use(morgan(':remote-addr [:method :url :http-version] :status'));
 
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1);
